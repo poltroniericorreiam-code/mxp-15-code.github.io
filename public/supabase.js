6b38148f5a516c0aa9f79843a94a901584e5b477
mxp15oficial <poltroniericorreiam@gmail.com>
2026-07-18 19:25:46 -0400

    Update supabase.js

--- public/supabase.js
+++ public/supabase.js
@@ -1,7 +1,11 @@
 const SUPABASE_URL = "https://guavczqfnqkshhgpyccs.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_1qUbWglTgDfCV4MIDT7qWQ_jetIG_ht";
 
+console.log(window.supabase);
+
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
+
+console.log(supabase);
