1e9d6f1da6e161673f0b376dc3966beb89912df9
mxp15oficial <poltroniericorreiam@gmail.com>
2026-07-18 19:31:30 -0400

    Improve logging for Supabase client initialization

--- public/supabase.js
+++ public/supabase.js
@@ -1,11 +1,12 @@
 const SUPABASE_URL = "https://guavczqfnqkshhgpyccs.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_1qUbWglTgDfCV4MIDT7qWQ_jetIG_ht";
 
-console.log(window.supabase);
+console.log("window.supabase =", window.supabase);
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
 
-console.log(supabase);
+console.log("Cliente =", supabase);
+console.log("Auth =", supabase.auth);
