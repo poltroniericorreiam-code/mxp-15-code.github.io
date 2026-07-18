8983f8640a3183e34aca236584226afa061318c3
mxp15oficial <poltroniericorreiam@gmail.com>
2026-07-18 09:37:47 -0400

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://guavczqfnqkshhgpyccs.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_1qUbWglTgDfCV4MIDT7qWQ_jetIG_ht";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
