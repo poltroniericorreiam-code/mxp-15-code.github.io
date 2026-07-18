4ac5017f9a970893986aff65e8d117d5b1af33e7
mxp15oficial <poltroniericorreiam@gmail.com>
2026-07-18 09:57:55 -0400

    Delete public/supabase.js

--- public/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://guavczqfnqkshhgpyccs.supabase.co";
-
-const SUPABASE_ANON_KEY = "sb_publishable_1qUbWglTgDfCV4MIDT7qWQ_jetIG_ht";
-
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
